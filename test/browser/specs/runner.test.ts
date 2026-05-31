e0aefb157a9a81482c70aab131e2dbb69c69c2c8
Whaletrucker Reef <scutua.tua@icloud.com>
2026-05-31 15:49:47 +0700

    Update runner.test.ts

--- test/browser/specs/runner.test.ts
+++ test/browser/specs/runner.test.ts
@@ -1,3 +1,11 @@
+const snapshot = timeoutErrorsIndexes.map((index) => {
+  return [
+    lines[index - 1].replace(/:(\d+):(\d+)$/, ':LINE:COL'),
+    lines[index].replace(/Timeout \d+ms exceeded/, 'Timeout <ms> exceeded'),
+    lines[index + 4] ? lines[index + 4].replace(/:(\d+):(\d+)$/, ':LINE:COL') : '',
+  ].join('\n')
+}).sort().join('\n\n')
+
 expect(snapshot).toMatchInlineSnapshot(`
 "FAIL |chromium| hooks-timeout.test.ts > timeouts are failing correctly > afterAll
 TimeoutError: locator.click: Timeout <ms> exceeded.
