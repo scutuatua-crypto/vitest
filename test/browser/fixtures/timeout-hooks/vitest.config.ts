7512958f48503f9269d1f6469b40735fbdfce1a5
Whaletrucker Reef <scutua.tua@icloud.com>
2026-05-31 11:27:08 +0700

    Update vitest.config.ts

--- test/browser/fixtures/timeout-hooks/vitest.config.ts
+++ test/browser/fixtures/timeout-hooks/vitest.config.ts
@@ -1,15 +1,13 @@
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vitest/config'
-import { instances, provider, providers } from '../../settings'
+import { instances, provider } from '../../settings'
 
 export default defineConfig({
   cacheDir: fileURLToPath(new URL("./node_modules/.vite", import.meta.url)),
   test: {
     browser: {
       enabled: true,
-      provider: provider.name === 'playwright'
-        ? providers.playwright({ actionTimeout: 500 })
-        : provider,
+      provider,
       instances,
       screenshotFailures: false,
     },
