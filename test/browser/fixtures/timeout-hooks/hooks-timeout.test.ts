4e925227dc2781462e1721e1f68176123b07319a
Whaletrucker Reef <scutua.tua@icloud.com>
2026-05-31 10:57:43 +0700

    Update hooks-timeout.test.ts

--- test/browser/fixtures/timeout-hooks/hooks-timeout.test.ts
+++ test/browser/fixtures/timeout-hooks/hooks-timeout.test.ts
@@ -1,17 +1,76 @@
-import { fileURLToPath } from 'node:url'
-import { defineConfig } from 'vitest/config'
-import { instances, provider, providers } from '../../settings'
+import { page, server } from 'vitest/browser';
+import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, it, onTestFailed, onTestFinished } from 'vitest';
 
-export default defineConfig({
-  cacheDir: fileURLToPath(new URL("./node_modules/.vite", import.meta.url)),
-  test: {
-    browser: {
-      enabled: true,
-      provider: provider.name === 'playwright'
-        ? providers.playwright({ actionTimeout: 500 })
-        : provider,
-      instances,
-      screenshotFailures: false,
-    },
-  },
+describe.runIf(server.provider === 'playwright')('timeouts are failing correctly', () => {
+  it('click on non-existing element fails', async () => {
+    await page.getByRole('code').click()
+  }, 150)
+
+  it('expect.element on non-existing element fails', async () => {
+    await expect.element(page.getByRole('code')).toBeVisible()
+  }, 150)
+
+  describe('beforeEach', () => {
+    beforeEach(async () => {
+      await page.getByTestId('non-existing').click()
+    }, 150)
+
+    it('skipped', () => {})
+  })
+
+  describe('afterEach', () => {
+    afterEach(async () => {
+      await page.getByTestId('non-existing').click()
+    }, 150)
+
+    it('skipped', () => {})
+  })
+
+  describe('beforeAll', () => {
+    beforeAll(async () => {
+      await page.getByTestId('non-existing').click()
+    }, 150)
+
+    it('skipped', () => {})
+  })
+
+  describe('afterAll', () => {
+    afterAll(async () => {
+      await page.getByTestId('non-existing').click()
+    }, 150)
+
+    it('skipped', () => {})
+  })
+
+  describe('onTestFinished', () => {
+    it('fails', ({ onTestFinished }) => {
+      onTestFinished(async () => {
+        await page.getByTestId('non-existing').click()
+      }, 150)
+    })
+
+    it('fails global', () => {
+      onTestFinished(async () => {
+        await page.getByTestId('non-existing').click()
+      }, 150)
+    })
+  })
+
+  describe('onTestFailed', () => {
+    it('fails', ({ onTestFailed }) => {
+      onTestFailed(async () => {
+        await page.getByTestId('non-existing').click()
+      }, 150)
+
+      expect.unreachable()
+    })
+
+    it('fails global', () => {
+      onTestFailed(async () => {
+        await page.getByTestId('non-existing').click()
+      }, 150)
+
+      expect.unreachable()
+    })
+  })
 })
