5ab543b4718a227efbe3e530832a87b1b9d239d2
Whaletrucker Reef <scutua.tua@icloud.com>
2026-05-31 10:56:11 +0700

    Update hooks-timeout.test.ts

--- test/browser/fixtures/timeout-hooks/hooks-timeout.test.ts
+++ test/browser/fixtures/timeout-hooks/hooks-timeout.test.ts
@@ -1,78 +1,17 @@
-import { page, server } from 'vitest/browser';
-import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, it, onTestFailed, onTestFinished } from 'vitest';
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vitest/config'
+import { instances, provider, providers } from '../../settings'
 
-const TIMEOUT = server.provider === 'playwright' && typeof process !== 'undefined' && process.platform === 'win32' ? 500 : 150
-
-describe.runIf(server.provider === 'playwright')('timeouts are failing correctly', () => {
-  it('click on non-existing element fails', async () => {
-    await page.getByRole('code').click()
-  }, TIMEOUT)
-
-  it('expect.element on non-existing element fails', async () => {
-    await expect.element(page.getByRole('code')).toBeVisible()
-  }, TIMEOUT)
-
-  describe('beforeEach', () => {
-    beforeEach(async () => {
-      await page.getByTestId('non-existing').click()
-    }, TIMEOUT)
-
-    it('skipped', () => {})
-  })
-
-  describe('afterEach', () => {
-    afterEach(async () => {
-      await page.getByTestId('non-existing').click()
-    }, TIMEOUT)
-
-    it('skipped', () => {})
-  })
-
-  describe('beforeAll', () => {
-    beforeAll(async () => {
-      await page.getByTestId('non-existing').click()
-    }, TIMEOUT)
-
-    it('skipped', () => {})
-  })
-
-  describe('afterAll', () => {
-    afterAll(async () => {
-      await page.getByTestId('non-existing').click()
-    }, TIMEOUT)
-
-    it('skipped', () => {})
-  })
-
-  describe('onTestFinished', () => {
-    it('fails', ({ onTestFinished }) => {
-      onTestFinished(async () => {
-        await page.getByTestId('non-existing').click()
-      }, TIMEOUT)
-    })
-
-    it('fails global', () => {
-      onTestFinished(async () => {
-        await page.getByTestId('non-existing').click()
-      }, TIMEOUT)
-    })
-  })
-
-  describe('onTestFailed', () => {
-    it('fails', ({ onTestFailed }) => {
-      onTestFailed(async () => {
-        await page.getByTestId('non-existing').click()
-      }, TIMEOUT)
-
-      expect.unreachable()
-    })
-
-    it('fails global', () => {
-      onTestFailed(async () => {
-        await page.getByTestId('non-existing').click()
-      }, TIMEOUT)
-
-      expect.unreachable()
-    })
-  })
+export default defineConfig({
+  cacheDir: fileURLToPath(new URL("./node_modules/.vite", import.meta.url)),
+  test: {
+    browser: {
+      enabled: true,
+      provider: provider.name === 'playwright'
+        ? providers.playwright({ actionTimeout: 500 })
+        : provider,
+      instances,
+      screenshotFailures: false,
+    },
+  },
 })
